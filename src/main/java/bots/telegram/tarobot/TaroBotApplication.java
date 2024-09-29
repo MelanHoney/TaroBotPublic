297d8e89706e117a83bdfa83a9ff9b692f9c7c6e
MelanHoney <vladtishenkoo@yandex.ru>
2024-09-30 00:49:27 +0300

    TaroBot 0.0.1

---
+++ src/main/java/bots/telegram/tarobot/TaroBotApplication.java
@@ -0,0 +1,13 @@
+package bots.telegram.tarobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaroBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaroBotApplication.class, args);
+	}
+
+}
